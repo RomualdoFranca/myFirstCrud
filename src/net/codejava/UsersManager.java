2b9168e41935e79ca992f356705d55902a49d421
RomualdoFranca <romualdo.ads2022@gmail.com>
2022-09-18 20:34:10 -0300

    input refatorado, realizado testes, atualizacao do BD

--- src/net/codejava/UsersManager.java
+++ src/net/codejava/UsersManager.java
@@ -17,37 +17,42 @@ public class UsersManager {
 //                "VALUES ('Romualdo', 'mamao','Romualdo Franca', 'romualdo@email');";
 //        Statement statement = connection.createStatement();
 
-//        System.out.println("Input username: ");
-//        String username = scanner.nextLine();
-//        System.out.println("Input password: ");
-//        String password = scanner.nextLine();
-//        System.out.println("Input fullname: ");
-//        String fullname = scanner.nextLine();
-//        System.out.println("Input email: ");
-//        String email = scanner.nextLine();
+        // Atualizando BD atraves de inputs do usuário
+        System.out.println("Input username: ");
+        String username = scanner.nextLine();
+        System.out.println("Input password: ");
+        String password = scanner.nextLine();
+        System.out.println("Input fullname: ");
+        String fullname = scanner.nextLine();
+        System.out.println("Input email: ");
+        String email = scanner.nextLine();
 
-        String username = "Lua";
-        String password = "peludos";
-        String fullname = "Lua Lora";
-        String email = "lua@email.com";
+        String sqlInputs = "INSERT INTO users(username, password, fullname, email) VALUES(?, ?, ?, ?)";
+        PreparedStatement statement1 = connection.prepareStatement(sqlInputs);
+        statement1.setString(1, username);
+        statement1.setString(2, password);
+        statement1.setString(3, fullname);
+        statement1.setString(4, email);
 
-        String sqlInputs = "INSERT INTO users (username, password, fullname, email)" +
-                "VALUES(?, ?, ?, ?)";
-        PreparedStatement statement = connection.prepareStatement(sqlInputs);
-        statement.setString(1, username);
-        statement.setString(2, password);
-        statement.setString(3, fullname);
-        statement.setString(4, email);
+        int rows =  statement1.executeUpdate();
 
-//        String sqlInputs = "INSERT INTO users(username, password, fullname, email) VALUES(?, ?, ?, ?)";
-//        PreparedStatement statement1 = connection.prepareStatement(sqlInputs);
-//        statement1.setString(1, username);
-//        statement1.setString(2, password);
-//        statement1.setString(3, fullname);
-//        statement1.setString(4, email);
+//      Atualizando BD sem Scanner
+//        String username = "Lua";
+//        String password = "peludos";
+//        String fullname = "Lua Lora";
+//        String email = "lua@email.com";
+
+//        String sql = "INSERT INTO users (username, password, fullname, email)" +
+//                "VALUES(?, ?, ?, ?)";
+//        PreparedStatement statement = connection.prepareStatement(sql);
+//        statement.setString(1, username);
+//        statement.setString(2, password);
+//        statement.setString(3, fullname);
+//        statement.setString(4, email);
+//        int rows =  statement.executeUpdate();
 
 
-        int rows =  statement1.executeUpdate(sqlInputs);
+
 
         if (rows > 0) {
             System.out.println("A new user has been inserted successfully");
